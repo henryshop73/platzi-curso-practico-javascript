c1a6844e74f0643d56ff994c9700e986a7410253
henryshop73 <henrymoranfreelance@gmail.com>
2022-07-05 20:05:38 -0500

    Creando función para calcular media aritmetica

--- promedio.js
+++ promedio.js
@@ -1,4 +1,4 @@
-const lista1 = [
+/* const lista1 = [
     100,
     200,
     300,
@@ -9,6 +9,17 @@ let sumaLista1 = 0
 for (let i = 0; i < lista1.length; i++) {
     sumaLista1 = sumaLista1 + lista1[i];
     
+} */
+
+/* const promedioLista1 = sumaLista1 / lista1.length; */
+
+function calcularMediaAritmetica(lista) {
+    let sumaLista = 0
+for (let i = 0; i < lista.length; i++) {
+    sumaLista = sumaLista + lista[i];   
 }
 
-const promedioLista1 = sumaLista1 / lista1.length;
+const promedioLista = sumaLista / lista.length;
+
+return promedioLista;
+}
