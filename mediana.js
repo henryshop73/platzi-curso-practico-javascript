68c0622d955dfafeef7e6dd3b5a1a1be848cef26
henryshop73 <henrymoranfreelance@gmail.com>
2022-07-07 02:22:08 -0500

    Creando función cuando la lista es par

--- mediana.js
+++ mediana.js
@@ -1,8 +1,17 @@
+function calcularMediaAritmetica(lista) {
+   const sumaLista = lista.reduce(
+      function (valorAcumulado = 0, nuevoElemento) {
+          return valorAcumulado + nuevoElemento
+      }
+  );
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+  } 
+
 const lista1 = [
     100,
     200,
     500,
-    800,
     400000000
 ];
 
@@ -19,11 +28,16 @@ function esPar(numerito) {
 let mediana;
 
 if (esPar(lista1.length)) {
-    // necesitamos dos elementos
-    // -> el promedio
-    // -> mediana
+    const elemento1 = lista1[mitadLista1 - 1];
+    const elemento2 = lista1[mitadLista1];
+
+    const promedioElemento1y2 = calcularMediaAritmetica([
+        elemento1,
+        elemento2
+    ]);
+      
+    mediana = promedioElemento1y2;
+    
 }else {
     mediana = lista1[mitadLista1];
-    // posición mitadLista1 dentro lista1
-    // -> mediana
 }
