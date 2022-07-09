76b29da35ca2cf3c279b20b8e2369f15ba509eee
henryshop73 <henrymoranfreelance@gmail.com>
2022-07-09 01:57:30 -0500

    Agregando los archivos de html y js de moda, creando una lista en un array y creando un objeto con map

---
+++ moda.js
@@ -0,0 +1,25 @@
+const lista1 = [
+    1,
+    2,
+    3,
+    1,
+    2,
+    3,
+    4,
+    2,
+    2,
+    2,
+    1
+]
+
+const lista1Count = {}
+
+lista1.map(
+    function (elemento) {
+        if (lista1Count[elemento]) {
+            lista1Count[elemento] += 1
+        }else {
+            lista1Count[elemento] = 1;
+        }       
+    }
+)
